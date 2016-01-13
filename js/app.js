212d906c37a8c8d3403665392f663377f4cb37e2
Jacob Smith <jacob@jacobsmith.io>
2016-01-13 10:46:21 -0600

    Implement session service
    `var session = angular.element(document).injector().get('session')`

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-angular.module('reClone', ['sky']);
+(function() {
+  function sessionFactory() {
+    var authenticated = false;
+
+    function authenticate() {
+      authenticated = true;
+      console.log('Authenticated');
+    }
+
+    function invalidate() {
+      authenticated = false;
+      console.log('Not Authenticated');
+    }
+
+    function isAuthenticated() {
+      return authenticated;
+    }
+
+    return {
+      authenticate: authenticate,
+      invalidate: invalidate,
+      isAuthenticated: isAuthenticated
+    };
+  }
+
+  angular.module('reClone', ['sky'])
+    .factory('session', sessionFactory);
+})();
