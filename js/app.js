674630cd2a18728d84025684783b3afe617d23cb
Jacob Smith <jacob@jacobsmith.io>
2016-01-13 19:56:47 -0600

    Implement modal component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 (function() {
+  function RunBlock(bbHelpConfig) {
+    bbHelpConfig.productId = 'REx';
+    bbHelpConfig.url = '//p1helpui.renxt.blackbaud.com/helpwidget.js';
+  }
+
   function sessionFactory() {
     var authenticated = false;
 
@@ -30,8 +35,47 @@
   }
   OmnibarController.$inject = ['session'];
 
+  function constituentStoreFactory() {
+    function getConstituent(id) {
+      return {
+        id: 1,
+        name: "Bill Murray",
+        image: "http://fillmurray.com/200/200"
+      };
+    }
+
+    return {
+      getConstituent: getConstituent
+    };
+  }
+
+  function ConstituentController(constituentStore, constituentId) {
+    this.constituent = constituentStore.getConstituent(constituentId);
+  }
+  ConstituentController.$inject = ['constituentStore', 'constituentId'];
+
+  function EditConstituentModalController(bbModal) {
+    this.open = function () {
+      bbModal.open({
+          controller: 'EditConstituentContentController as editConstituentVm',
+          templateUrl: 'constituents/edit.html'
+      });
+    };
+  }
+  EditConstituentModalController.$inject = ['bbModal'];
+
+  function EditConstituentContentController(constituentStore, constituentId) {
+    this.constituent = constituentStore.getConstituent(constituentId);
+  }
+  EditConstituentContentController.$inject = ['constituentStore', 'constituentId'];
 
   angular.module('reClone', ['sky'])
+    .value('constituentId', 1)
     .factory('session', sessionFactory)
+    .factory('constituentStore', constituentStoreFactory)
     .controller('OmnibarController', OmnibarController)
+    .controller('ConstituentController', ConstituentController)
+    .controller('EditConstituentModalController', EditConstituentModalController)
+    .controller('EditConstituentContentController', EditConstituentContentController)
+    .run(RunBlock);
 })();
